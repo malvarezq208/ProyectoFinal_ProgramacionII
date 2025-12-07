7c305a312e1c5ada19261007d65c056b5f5e29ac
malvarezq208 <304860392@cuc.cr>
2025-12-07 14:28:46 -0600

    Feat:Actualizacion GestorBaseDatos
    Creacion de la BD y tablas pais y clima con su respectivo metodo (Creacion,inserccion y consultas)

--- scr/main.py
+++ scr/main.py
@@ -2,6 +2,8 @@
 from api.api import ClienteAPI
 from datos.gestor_datos import GestorDatos
 from eda.procesador_eda import ProcesadorEDA as eda
+from basedatos.gestor_basedatos import BD
+import pandas as pd
 
 #----------------------------------------------------------------------------------------------------------------------#
 # Clase = ClienteAPI / Instancia para consultar las API's
@@ -26,3 +28,63 @@ eda_turismo.ejecutar_eda('turismo_anios_clean.csv')
 
 eda_zonas = eda(zonas.df)
 eda_zonas.ejecutar_eda('zonas_aereas_clean.csv')
+
+#----------------------------------------------------------------------------------------------------------------------#
+#Ejecucion base datos
+
+#----------------------------------------------------------------------------------------------------------------------#
+#Carga de los archivos CSV processed
+
+#Archivo clima_anual_2013.csv
+clima_anual=GestorDatos('data/processed/clima_anual_2013.csv')
+df_clima_anual=pd.DataFrame(clima_anual.retornar_csv())
+
+
+#Archivo clima_resumen_anual.csv
+clima_resumen_anual=GestorDatos('data/processed/clima_resumen_anual.csv')
+df_clima_resumen=pd.DataFrame(clima_resumen_anual.retornar_csv())
+
+
+#Archivo Coordenadas_paises.csv
+paises=GestorDatos('data/processed/Coordenadas_paises.csv')
+df_paises=pd.DataFrame(paises.retornar_csv())
+
+
+#Archivo turismo_anios_clean.csv
+turismo_anual=GestorDatos('data/processed/turismo_anios_clean.csv')
+df_turismo_anual=pd.DataFrame(turismo_anual.retornar_csv())
+
+
+#Archivo zonas_aereas_clean.csv
+zonas_aereas=GestorDatos('data/processed/zonas_aereas_clean.csv')
+df_zonas_aereas=pd.DataFrame(zonas_aereas.retornar_csv())
+
+
+#----------------------------------------------------------------------------------------------------------------------#
+#Creacion base datos
+basedatos=BD("MigracionCR")
+basedatos.crear_tabla()
+
+#----------------------------------------------------------------------------------------------------------------------#
+#Insert de las tablas
+#Tabla Paises
+basedatos.insertar_paises(df_paises)
+
+#Tabla Clima
+basedatos.cargar_clima(df_clima_resumen,44)
+
+#----------------------------------------------------------------------------------------------------------------------#
+#Consulta de las tablas
+
+#Consulta Tabla Pais
+consulta_pais=basedatos.consultar_tabla('Pais')
+print(f"Tabla Pais \n {consulta_pais}")
+
+#Consulta Tabla Clima Anual Costa Rica
+consulta_clima=basedatos.consultar_tabla('Clima')
+print(f"Tabla Clima Anual CR \n {consulta_clima}")
+
+
+
+
+
